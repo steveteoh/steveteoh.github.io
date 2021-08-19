960bd236538c3a836a9fd0155156554a66ea2fbc
Steve Teoh <cheehteoh@gmail.com>
2021-08-19 16:26:52 +0800

    Add files via upload

--- HexGrid/script.js
+++ HexGrid/script.js
@@ -6,19 +6,19 @@ var bounds;
 
 var places = [
    //vertical hex
-  [3.04728, 101.78039],        ///northwestx2
+  [3.04728, 101.78042],        ///northwestx2
   [3.04339, 101.78725],        //northwest
   [3.05503, 101.7940],        //northx2
   [3.04726, 101.7940],        //north
-  [3.04339, 101.80081],        //northeast
-  [3.04728, 101.8076],        //northeastx2
+  [3.04339, 101.80080],        //northeast
+  [3.04728, 101.80755],        //northeastx2
   [3.03950, 101.7940],        //UTAR - centre
-  [3.03174, 101.78039],        //southwestx2
+  [3.03173, 101.78042],        //southwestx2
   [3.03562, 101.78725],        //southwest
   [3.02399, 101.7940],        //southx2
   [3.03174, 101.7940],        //south
-  [3.03562, 101.80081],        //southeast
-  [3.03174, 101.8076],        ///southeastx2
+  [3.03562, 101.80080],        //southeast
+  [3.03173, 101.80755],        ///southeastx2
 
   /* Horizontal hex - not used
   [3.0395, 101.7784],
