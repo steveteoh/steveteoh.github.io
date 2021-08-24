d5dbbd4e95dd4bd6d5da5743e6ab79e61a3b2c90
Steve Teoh <cheehteoh@gmail.com>
2021-08-24 14:15:04 +0800

    Add files via upload

--- HexGrid/script.js
+++ HexGrid/script.js
@@ -20,7 +20,7 @@ const PLACE_BOUNDS = {
 
 const green = 'rgb(0, 255, 0)';     //for less than 10 cases
 const yellow = 'rgb(255, 255, 0)';  //for 11 - 50 cases
-const orange = 'rgb(255, 192, 0)';  //for 50 - 99 cases
+const orange = 'rgb(255, 72, 0)';  //for 50 - 99 cases
 const red = 'rgb(255, 0, 0)';       //for > 100 active cases
 const greenlvl = 10;
 const yellowlvl = 50;
