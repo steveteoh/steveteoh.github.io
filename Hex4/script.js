e321e55744b06d09e6e0e66f662a10958fde29bb
steveteoh <cheehteoh@gmail.com>
2021-09-01 13:04:54 +0800

    ver 4.1.2

--- Hex4/script.js
+++ Hex4/script.js
@@ -210,7 +210,7 @@ $(window).load(function () {
                         pos = { lat: lt1, lng: ln1 };
                         if (isInside(mygeometry, pos) == true) {
                             counter++;
-                            var locationname = geocodeLatLng(geocoder, map, lt1, ln1);
+                            var locationname = geocodeLatLng(geocoder, map, pos);
                             var label1 = "Daerah: " + mapID + "<br>No:" + counter + "<br>Hex coord:(" + (2 * l).toString() + ";" + (k).toString() + ")";
                             var weeklyactive = Math.floor(Math.random() * 101); // generates a random integer from 0 to 100:
                             var totalactive = Math.floor(Math.random() * 1001); // generates a random integer from 0 to 1000:
@@ -225,7 +225,7 @@ $(window).load(function () {
                         pos = { lat: lt2, lng: ln2 };
                         if (isInside(mygeometry, pos) == true) {
                             counter++;
-                            var locationname = geocodeLatLng(geocoder, map, lt2, ln2);
+                            var locationname = geocodeLatLng(geocoder, map, pos);
                             var label2 = "Daerah: " + mapID + "<br>No:" + counter + "<br>Hex coord:(" + (2 * l + 1).toString() + ";" + (k).toString() + ")";
                             var weeklyactive = Math.floor(Math.random() * 101); // generates a random integer from 0 to 100:
                             var totalactive = Math.floor(Math.random() * 1001); // generates a random integer from 0 to 1000:
@@ -254,7 +254,7 @@ $(window).load(function () {
                 });
                 marker.setIcon("https://maps.google.com/mapfiles/ms/icons/blue.png");
                 //Attaching related information onto the marker
-                attachMessage(marker, place[2] + ' : ' + place[3] +
+                attachMessage(marker, place[2] + ' : place name: ' + place[3] +
                     '<br>Coordinates: (' + place[0] + ',' + place[1] + ')' +
                     '<br>Weekly Active cases: ' + place[4] +
                     '       | Total Active cases: ' + place[5] +
@@ -302,19 +302,14 @@ $(window).load(function () {
     });
 });
 
-function geocodeLatLng(geocoder, map, lt, ln) {
-    const latlng = {
-        lat: lt,
-        lng: ln,
-    };
-    geocoder.geocode(latlng)
+function geocodeLatLng(geocoder, map, pos) {
+    geocoder.geocode(pos)
         .then((response) => {
             if (response.results[0]) {
                 map.setZoom(11);
                 return response.results[0].formatted_address;
             }
             else {
-
                 return "place name n/a";
             }
         })
