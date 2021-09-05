6209c5f459b623c7e2e549884aba1e73f3e069b3
steveteoh <cheehteoh@gmail.com>
2021-09-06 04:20:46 +0800

    trial 2

--- Hex4.2.5.1/script.js
+++ Hex4.2.5.1/script.js
@@ -33,8 +33,8 @@ var stateRequestURL = 'https://steveteoh.github.io/Hex4.2.5.1/Selangor/selangor.
 //var districtRequestURL = 'https://steveteoh.github.io/Hex4.2.5.1/Selangor/daerah/petaling_jaya.json';
 //var districtRequestURL = 'https://steveteoh.github.io/Hex4.2.5.1/Selangor/daerah/hulu_selangor.json';
 //var districtRequestURL = 'https://steveteoh.github.io/Hex4.2.5.1/Selangor/daerah/ampang_jaya.json';
-//var districtRequestURL = 'https://steveteoh.github.io/Hex4.2.5.1/Selangor/daerah/kuala_selangor.json';
-var districtRequestURL = 'https://steveteoh.github.io/Hex4.2.5.1/Selangor/daerah/sepang.json';
+var districtRequestURL = 'https://steveteoh.github.io/Hex4.2.5.1/Selangor/daerah/kuala_selangor.json';
+//var districtRequestURL = 'https://steveteoh.github.io/Hex4.2.5.1/Selangor/daerah/sepang.json';
 //var districtRequestURL = 'https://steveteoh.github.io/Hex4.2.5.1/Selangor/daerah/kuala_langat.json';
 //var districtRequestURL = 'https://steveteoh.github.io/Hex4.2.5.1/Selangor/daerah/klang.json';
 //var districtRequestURL = 'https://steveteoh.github.io/Hex4.2.5.1/Selangor/daerah/hulu_langat.json';
@@ -46,8 +46,8 @@ var districtRequestURL = 'https://steveteoh.github.io/Hex4.2.5.1/Selangor/daerah
 //var mapID = "Petaling Jaya";
 //var mapID = "Hulu Selangor";
 //var mapID = "Ampang Jaya";
-//var mapID = "Kuala Selangor";
-var mapID = "Sepang";         //NOTE: - isinside does not work with holes (putrajaya) yet...revising
+var mapID = "Kuala Selangor";
+//var mapID = "Sepang";         //NOTE: - isinside does not work with holes (putrajaya) yet...revising. Alternative - visual method
 //var mapID = "Kuala Langat";   
 //var mapID = "Klang";          //MULTI - need to adjust the geojson boundary for pulau
 //var mapID = "Hulu Langat";    //MULTI
@@ -91,16 +91,16 @@ const PLACE_BOUNDS = {
     //south: 3.081443,
     //west: 101.733063,
     //east: 101.853560,
-    //name: "Kuala Selangor",
-    //north: 3.600000,  
-    //south: 3.158670,
-    //west: 101.10080,
-    //east: 101.49380,
-    name: "Sepang",
-    north: 3.012039,
-    south: 2.594652,
-    west: 101.589953,
-    east: 101.78966,
+    name: "Kuala Selangor",
+    north: 3.600000,  
+    south: 3.158670,
+    west: 101.10080,
+    east: 101.49380,
+    //name: "Sepang",
+    //north: 3.012039,
+    //south: 2.594652,
+    //west: 101.589953,
+    //east: 101.78966,
     //name: "Kuala Langat",
     //north: 2.978663,
     //south: 2.643984,
