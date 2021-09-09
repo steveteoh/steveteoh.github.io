382a590411cdced68c9111488d2602c443c506ab
steveteoh <cheehteoh@gmail.com>
2021-09-09 22:43:17 +0800

    seberang perai

--- Penang/script.js
+++ Penang/script.js
@@ -25,13 +25,13 @@ const MAP_BOUNDS = {
 
 //Administrative boundary file - geojson (sourced from: https://github.com/TindakMalaysia/Selangor-Maps)
 var stateRequestURL = 'https://steveteoh.github.io/Maps/Penang/penang.json';
-var districtRequestURL = 'https://steveteoh.github.io/Maps/Penang/island.json';
-var mapID = "Penang";
+var districtRequestURL = 'https://steveteoh.github.io/Maps/Penang/seberangperai.json';
+var mapID = "Seberang Perai";
 
 // Places are automatically generated using just north, south, east and west boundary coordinates. 
 // E.g. Hulu Langat, Selangor (not yet according to map shape. Future version will include precise kmz boundaries)
 const PLACE_BOUNDS = {
-    name: "Penang",
+    name: "Seberang Perai",
     north: 5.482900,
     south: 5.257000,
     west: 100.177000,
